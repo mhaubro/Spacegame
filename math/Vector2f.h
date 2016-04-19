227aee898cc58024c7d9daada7e9284c2d78bfe5
Mathias Gammelmark <mathiasgam@hotmail.com>
2016-04-19 14:57:47 +0200

    Vector initial

---
+++ math/Vector2f.h
@@ -0,0 +1,33 @@
+/*
+ * Vector2f.h
+ *
+ *  Created on: Apr 19, 2016
+ *      Author: mathi
+ */
+
+#ifndef SRC_MATH_VECTOR2F_H_
+#define SRC_MATH_VECTOR2F_H_
+
+class Vector2f {
+public:
+	float x,y;
+	Vector2f();
+	Vector2f(float x, float y);
+	virtual ~Vector2f();
+
+	float length();
+	float angle();
+
+	Vector2f& operator=(const Vector2f& v);
+	Vector2f& operator+(const Vector2f& v);
+	Vector2f& operator-(const Vector2f& v);
+	Vector2f& operator+=(const Vector2f& v);
+	Vector2f& operator-=(const Vector2f& v);
+
+	Vector2f& operator*(const float a);
+	Vector2f& operator/(const float a);
+	Vector2f& operator*=(const float a);
+	Vector2f& operator/=(const float a);
+};
+
+#endif /* SRC_MATH_VECTOR2F_H_ */
