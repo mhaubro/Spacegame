7469dd76e1b1d506bdc2890f5dec14a8babf9cd6
Mathias Gammelmark <mathiasgam@hotmail.com>
2016-04-20 11:10:29 +0200

    Chunks
    made simple chunk generation and rewrite function. and fixed the render
    function to discard vertexes to far off the screen

--- environment/Chunk.h
+++ environment/Chunk.h
@@ -8,18 +8,22 @@
 #ifndef SRC_ENVIRONMENT_CHUNK_H_
 #define SRC_ENVIRONMENT_CHUNK_H_
 
-#define CHUNK_SIZE 64
+#define CHUNK_SIZE 16
+#define VERTEX_PER_UNIT 2
+#define VERTEX_NUMBER (CHUNK_SIZE * VERTEX_PER_UNIT + 1)
+#define VERTEX_SEPERATION (((float) 1) / VERTEX_PER_UNIT)
 
 class Chunk {
 protected:
 
-	short index;
-	short heightMap [CHUNK_SIZE];
+	int index;
+	float heightMap[VERTEX_NUMBER];
 
 public:
-	Chunk(short _index);
+	Chunk(int _index);
 	virtual ~Chunk();
 	void render();
+	void rewrite(int index);
 
 };
 
---
+++ math/Random.cpp
@@ -0,0 +1,26 @@
+/*
+ * Random.cpp
+ *
+ *  Created on: Apr 20, 2016
+ *      Author: mathi
+ */
+
+#include "Random.h"
+
+Random ran;
+
+Random::Random() {
+
+}
+
+Random::~Random() {
+	// TODO Auto-generated destructor stub
+}
+
+float Random::randomF(int x){
+	static short v1[] = {5,2,8,2,1,9,6,3,9,1,3,8,4,2,8,5,9,3,5,4};
+	static short v2[] = {12, 5, 0, 2, 7, 9};
+	int y = (x >> 1) ^ (-(x & 1u) & 0xB400u);
+	return ((float) 1) / v1[(y+(v2[v1[x]%6]))%20];
+}
+
