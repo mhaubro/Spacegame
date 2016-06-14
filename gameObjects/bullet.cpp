5981d3bf289a587366cb7ee1175da7c329eb78f6
Mathias Gammelmark <mathiasgam@hotmail.com>
2016-06-14 10:04:43 +0200

    explosions :collision:

--- gameObjects/bullet.cpp
+++ gameObjects/bullet.cpp
@@ -4,7 +4,6 @@
 #include "game.h"
 #include "PhysicsConstants.h"
 
-
 #include <vector>
 #include <algorithm>
 
@@ -14,13 +13,13 @@ std::vector<Bullet> friendlybullets;
 std::vector<Bullet> foebullets;
 
 Bullet::Bullet(Vector2f& pos, Vector2f& vel, float radius, bool _friendly) :
-		Entity(), PhysicsObject(BULLET_MASS, pos, vel), startTime(timer.getRunTime()), mFriendly(_friendly), mRadius(radius), mAnimation(
+		Entity(), PhysicsObject(BULLET_MASS, pos, vel), startTime(
+				timer.getRunTime()), mFriendly(_friendly), mRadius(radius), mAnimation(
 				BulletTemplate, pos, 0, 2) {
 
 }
 
-
-Bullet& Bullet::operator=(const Bullet& b){//PROBABLY UNNECESSARY DUE TO DECISION TO USE POINTERS
+Bullet& Bullet::operator=(const Bullet& b) { //PROBABLY UNNECESSARY DUE TO DECISION TO USE POINTERS
 	this->velocity = b.velocity;
 	this->position.x = b.position.x;
 	this->position.y = b.position.y;
@@ -41,16 +40,20 @@ void Bullet::update() {
 		mIsDead = true;
 	}
 
-	game.mEnemyManager.checkBulletCollision(this);
+	if (game.mEnemyManager.checkBulletCollision(this)) {
+		game.mEffectManager.addEffect(
+				new StaticAnimationEffect(position, .4,
+						BulletCollisionAnimation32, velocity.angle(), 1));
 
-	if (checkEarthCollision()) {
+	} else if (checkEarthCollision()) {
 		Vector2f normal;
 		world.getNormal(position.x, normal);
 
-		StaticAnimationEffect* effect = new StaticAnimationEffect(
-				Vector2f(position.x,world.getHeight(position.x)) + normal, .8, GroundCollisionAnimation32,
-				normal.angle() + PI / 2, 1);
-		game.mEffectManager.addEffect(effect);
+		game.mEffectManager.addEffect(
+				new StaticAnimationEffect(
+						Vector2f(position.x, world.getHeight(position.x))
+								+ normal, .8, GroundCollisionAnimation32,
+						normal.angle() + PI / 2, 1));
 		kill();
 	}
 
@@ -60,23 +63,22 @@ bool Bullet::checkEarthCollision() {	//Simplified
 	return (position.y < world.getHeight(position.x) + mRadius);
 }
 
-
-Vector2f getShortestDiffVectorBullet(Vector2f v1, Vector2f v2){//Shortest vector that points from v1 to v2, with respect to the world construction
-	Vector2f dv = v2-v1;//Difference vector from v1 to v2.
-	if (dv.x < -CHUNK_SIZE*WORLD_SIZE/2){
-		dv.x += CHUNK_SIZE*WORLD_SIZE;
-	} else if(dv.x > CHUNK_SIZE*WORLD_SIZE/2){
-		dv.x -= CHUNK_SIZE*WORLD_SIZE;
+Vector2f getShortestDiffVectorBullet(Vector2f v1, Vector2f v2) {//Shortest vector that points from v1 to v2, with respect to the world construction
+	Vector2f dv = v2 - v1;	//Difference vector from v1 to v2.
+	if (dv.x < -CHUNK_SIZE * WORLD_SIZE / 2) {
+		dv.x += CHUNK_SIZE * WORLD_SIZE;
+	} else if (dv.x > CHUNK_SIZE * WORLD_SIZE / 2) {
+		dv.x -= CHUNK_SIZE * WORLD_SIZE;
 	}
 	return dv;
 }
 
-bool Bullet::outOfBounds(){
+bool Bullet::outOfBounds() {
 	Vector2f camv = Vector2f();
 	camv.x = cam.getX();
 	camv.y = cam.getY();
 	Vector2f diff = getShortestDiffVectorBullet(camv, position);
-	return diff.length() > CHUNK_SIZE*1.4;
+	return diff.length() > CHUNK_SIZE * 1.4;
 }
 
 bool Bullet::isDead() {
