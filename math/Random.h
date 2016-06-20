16b8742232cba7b536572cc94522b197aa557a85
Mathias Gammelmark <mathiasgam@hotmail.com>
2016-06-20 11:02:39 +0200

    Better Random function

--- math/Random.h
+++ math/Random.h
@@ -5,15 +5,21 @@
 
 class Random {
 protected:
+	unsigned char recVal;
+	unsigned char index;
+	char getRecVal();
 
 public:
 	Random();
 	virtual ~Random();
 
 	float Float(int x);
+	float FloatOLD(int x);
+	float FloatRec();
+
+	void setSeed(int x);
 };
 
 extern Random ran;
-
 
 #endif /* SRC_MATH_RANDOM_H_ */
